e3f39bbc2b35e8f72858bad8a1b95d7ccae70531
Alex Robinson <m.alexanderrobinson@yahoo.com>
2021-03-29 14:25:32 -0500

    add py_gatt.py

---
+++ py_gatt.py
@@ -0,0 +1,136 @@
+from array import array
+import gatt
+import os
+from gi.repository import GObject, Gio, GLib
+from util import *
+
+
+class AnyDeviceDFU(gatt.Device):
+    # Class constants
+    UUID_DFU_SERVICE = "00001530-1212-efde-1523-785feabcd123"
+    UUID_CTRL_POINT = "00001531-1212-efde-1523-785feabcd123"
+    UUID_PACKET = "00001532-1212-efde-1523-785feabcd123"
+    UUID_VERSION = "00001534-1212-efde-1523-785feabcd123"
+
+    def connect_succeeded(self):
+        super().connect_succeeded()
+        print("[%s] Connected" % (self.mac_address))
+
+    def connect_failed(self, error):
+        super().connect_failed(error)
+        print("[%s] Connection failed: %s" % (self.mac_address, str(error)))
+
+    def disconnect_succeeded(self):
+        super().disconnect_succeeded()
+        print("[%s] Disconnected" % (self.mac_address))
+
+    def services_resolved(self):
+        super().services_resol
+        ble_dfu_serv = next(
+            s for s in self.services if s.uuid == self.UUID_DFU_SERVICE
+        )
+        ctrl_point_char = next(
+            c for c in ble_dfu_serv.characteristics if c.uuid == self.UUID_CTRL_POINT
+        )
+        packet_char = next(
+            c for c in ble_dfu_serv.characteristics if c.uuid == self.UUID_PACKET
+        )
+        # Write "Start DFU" (0x01) to DFU Control Point
+        # Write the image size to DFU Packet 
+        # <Length of SoftDevice><Length of bootloader><Length of application>
+        # lengths must be in uint32
+
+
+class InfiniTimeManager(gatt.DeviceManager):
+    def __init__(self):
+        cmd = "btmgmt info"
+        btmgmt_proc = Gio.Subprocess.new(
+            cmd.split(),
+            Gio.SubprocessFlags.STDIN_PIPE | Gio.SubprocessFlags.STDOUT_PIPE,
+        )
+        _, stdout, stderr = btmgmt_proc.communicate_utf8()
+        self.adapter_name = stdout.splitlines()[1].split(":")[0]
+        self.alias = None
+        self.scan_result = False
+        self.mac_address = None
+        super().__init__(self.adapter_name)
+
+    def get_scan_result(self):
+        return self.scan_result
+
+    def get_mac_address(self):
+        return self.mac_address
+
+    def set_timeout(self, timeout):
+        GObject.timeout_add(timeout, self.stop)
+
+    def device_discovered(self, device):
+        if device.alias() in ("InfiniTime", "Pinetime-JF"):
+            self.alias = device.alias()
+            self.scan_result = True
+            self.mac_address = device.mac_address
+            self.stop()
+
+    def scan_for_infinitime(self):
+        self.start_discovery()
+        self.set_timeout(5 * 1000)
+        self.run()
+
+
+class InfiniTimeOTA(gatt.Device):
+    # Class constants
+    UUID_CONTROL_POINT = "00001531-1212-efde-1523-785feabcd123"
+    UUID_PACKET = "00001532-1212-efde-1523-785feabcd123"
+    UUID_VERSION = "00001534-1212-efde-1523-785feabcd123"
+
+    def __init__(self, mac_address, manager, firmware_path, datfile_path):
+        self.target_mac = mac_address
+
+        self.firmware_path = firmware_path
+        self.datfile_path = datfile_path
+        super().__init__(mac_address, manager)
+
+    def input_setup(self):
+        print(
+            "Sending file "
+            + os.path.split(self.firmware_path)[1]
+            + " to "
+            + self.target_mac
+        )
+
+        if self.firmware_path == None:
+            raise Exception("input invalid")
+
+        name, extent = os.path.splitext(self.firmware_path)
+
+        if extent == ".bin":
+            self.bin_array = array("B", open(self.firmware_path, "rb").read())
+
+            self.image_size = len(self.bin_array)
+            print("Binary imge size: %d" % self.image_size)
+            print(
+                "Binary CRC32: %d" % crc32_unsigned(array_to_hex_string(self.bin_array))
+            )
+
+            return
+
+        if extent == ".hex":
+            intelhex = IntelHex(self.firmware_path)
+            self.bin_array = intelhex.tobinarray()
+            self.image_size = len(self.bin_array)
+            print("bin array size: ", self.image_size)
+            return
+
+        raise Exception("input invalid")
+
+    def connect_succeeded(self):
+        super().connect_succeeded()
+        print("[%s] Connected" % (self.mac_address))
+
+    def connect_failed(self, error):
+        super().connect_failed(error)
+        print("[%s] Connection failed: %s" % (self.mac_address, str(error)))
+
+    def disconnect_succeeded(self):
+        super().disconnect_succeeded()
+        print("[%s] Disconnected" % (self.mac_address))
